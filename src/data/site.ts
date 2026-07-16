e9496063c784699e00ae15a53ab71df550059689
D. Henschel <141463151+dhenschel@users.noreply.github.com>
2026-07-16 18:56:14 +0200

    add selectable startup profiles

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,7 +1,7 @@
 export const siteProfile = {
   name: "dhenschel",
   role: "Software Developer",
-  image: "/profile-placeholder.svg",
+  image: "/dhenschel.jpg?v=profile-2026",
   imageAlt: "Profilbild von D. Henschel",
   github: "https://github.com/dhenschel",
   contactEmail: "danielhenschel04@gmail.com",
